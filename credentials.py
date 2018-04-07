1dbe94159d389d242fabf4aff4a038dd6322ee3d
pelovett <pelovett@gmail.com>
2018-04-07 11:02:27 -0700

    Updated credential name conventions

--- credentials.py
+++ credentials.py
@@ -1,8 +1,8 @@
 #Name of the Mongodb file
-DB_NAME = ""
+DB_NAME = "test"
 #Database user name
-DB_USER = ""
+DB_USER = "admin"
 #Password for the specified user
-DB_PASSWORD = ""
+DB_PASSWORD = "HackCause"
 #Address of Atlas Mongodb Instance
-DB_DOMAIN = "" 
+DB_DOMAIN = "cluster0-eouik.mogodb.net" 
