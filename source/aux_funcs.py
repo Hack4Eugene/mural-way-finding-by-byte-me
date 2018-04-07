ac5856e7c727111cf2fc20076ac009d3ac04584c
Jeremiah Clothier <jclothie@uoregon.edu>
2018-04-07 11:06:18 -0700

    skeleton for add_mural

--- source/aux_funcs.py
+++ source/aux_funcs.py
@@ -1,3 +1,5 @@
+import pymongo
+from pymongo import MongoClient
 
 def euclidean(x, y):
     sumSq = 0.0
@@ -8,3 +10,17 @@ def euclidean(x, y):
 
     # take the square root of the result
     return sumSq ** 0.5
+
+def add_mural(db,image,title,address,description):
+	"""
+	@brief      Adds mural to database.
+	
+	@param      db           The database
+	@param      image        The image
+	@param      title        The title of mural
+	@param      address      The address of mural
+	@param      description  The description for mural
+	
+	@return     None, updates the database with a new mural entry
+	"""
+	return None
