ff9cc21740d696889b5e6b1876f19fa605c43c78
bearwithscissors <wyatt@wyattreed.com>
2018-04-07 16:08:05 -0700

    added credentials

--- credentials.py
+++ credentials.py
@@ -4,3 +4,6 @@ DB_USER = "admin"
 DB_PASSWORD = "test"
 #Address of Atlas Mongodb Instance
 DB_DOMAIN = "cluster0-shard-00-00-eouik.mongodb.net:27017,cluster0-shard-00-01-eouik.mongodb.net:27017,cluster0-shard-00-02-eouik.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin"
+
+AWSAccessKeyId = 'AKIAJKNTY5AMTWSHWWMQ'
+AWSSecretKey = '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'
