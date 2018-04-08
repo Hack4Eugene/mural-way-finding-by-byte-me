2107577f6877b0dc2c9362c0fd93675a3f889a14
pelovett <pelovett@gmail.com>
2018-04-07 23:07:13 -0700

    Added mural processing function

--- DB.py
+++ DB.py
@@ -67,12 +67,11 @@ def add_selfie_to_queue(db,img_id,mural_id):
     @return     None, adds a selfie entry to db["AdminSelfieQ"]
     """
 
-    db = db["AdminSelfieQ"]
     entry = {
         "img_id": img_id,
         "mural_id": mural_id
     }
-    db.insert(entry)
+    db["AdminSelfieQ"].insert(entry)
     return None
 
 
@@ -89,7 +88,6 @@ def process_selfie(db, is_approved, aws_url):
 
     selfie = db["AdminSelfieQ"].find_one({"img_id":aws_url})
     mural_id = selfie["mural_id"]
-    print(mural_id)
     if is_approved:
         obj = objectid.ObjectId(mural_id)
         mural = db["Mural"].find_one({"_id":obj})
@@ -103,6 +101,7 @@ def process_selfie(db, is_approved, aws_url):
     db["AdminSelfieQ"].remove({"img_id":aws_url})
     return None
 
+
 def process_mural(db, is_approved, aws_url):
     """
     @brief      process a mural in the admin's mural queue
@@ -114,4 +113,10 @@ def process_mural(db, is_approved, aws_url):
     @return     None,         Delete the value from the queue and add the mural to the table
     """
 
-    selfie = db["AdminMuralQ"]
+    new = db["AdminMuralQ"].find_one({"img_id":aws_url})
+    result = db["Mural"].insert_one(new)
+    if result == None:
+        print("Failed to add to mural queue")
+
+    db["AdminMuralQ"].delete_one({"img_id":aws_url})
+    return 
