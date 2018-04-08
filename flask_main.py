ca009aa82568ac1c4d5706b1362524778fe3a504
Noah Palmer <npalmer@uoregon.edu>
2018-04-07 23:17:39 -0700

    HELAO

--- flask_main.py
+++ flask_main.py
@@ -30,6 +30,9 @@ app.secret_key = str(uuid.uuid4())
 @app.route("/")
 @app.route("/index")
 def index():
+    if 'admin_status' not in flask.session:
+        flask.session['admin_status'] = False
+        
     app.logger.debug("Main page entry")
     return render_template('index.html')
 
