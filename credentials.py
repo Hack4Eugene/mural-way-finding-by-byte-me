9577be3dd5d414fdeb8c578a9b957e70e110ff02
Jeremiah Clothier <jclothie@uoregon.edu>
2018-04-08 03:21:45 -0700

    help me

--- credentials.py
+++ credentials.py
@@ -1,13 +1,13 @@
 #Database user name
-DB_USER = ""
+DB_USER = "admin"
 #Password for the specified user
-DB_PASSWORD = ""
+DB_PASSWORD = "test"
 #Address of Atlas Mongodb Instance
-DB_DOMAIN = ""
+DB_DOMAIN = "cluster0-shard-00-00-eouik.mongodb.net:27017,cluster0-shard-00-01-eouik.mongodb.net:27017,cluster0-shard-00-02-eouik.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin"
 
-AWSAccessKeyId = ''
-AWSSecretKey = ''
+AWSAccessKeyId = 'AKIAJKNTY5AMTWSHWWMQ'
+AWSSecretKey = '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'
 
 #Admin user/pass
-ADMIN_ID = ""
-ADMIN_PW = ""
+ADMIN_ID = "obama"
+ADMIN_PW = "bidenB0i"
